cf8d967639062dbbe398aec6f191fcc65217f9f0
Franklin Vale <franklin18@ru.is>
2018-12-05 13:57:32 +0000

    Some file changes

--- main/gui/services/login.py
+++ main/gui/services/login.py
@@ -1,7 +1,5 @@
 import sys
-# Add the current directory to `sys.path`.
-if sys.path[0] != '':
-    sys.path.insert(0, '')
+
 class loginVerification:
     def __init__(self, username, password):
         self.__username = username
