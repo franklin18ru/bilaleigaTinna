348b5ce9da7b98dd3d9216b60664926727d690b7
mariamjoll1 <mariahrafns@gmail.com>
2018-12-06 16:03:44 +0000

    update on order_car_dateUi

--- GUI/order_car_menuCarsUi.py
+++ GUI/order_car_menuCarsUi.py
@@ -9,52 +9,49 @@ root.configure(bg="#5A6D7C")
 root.geometry(str(screen_width)+"x"+str(screen_height))
 
 
-
 #Create labels
 bilaleigaTinna = Label(root, text="Bílaleiga Tinna",bg="#5A6D7C",fg="white")
-label1 = Label(root, text="Velja dagsetningu",bg="#5A6D7C",fg="white")
+label1 = Label(root, text="Panta bíl",bg="#5A6D7C",fg="white")
 line1 = Label(root, text="____________________________",bg="#5A6D7C",fg="white")
 line2 = Label(root, text="____________________________",bg="#5A6D7C",fg="white")
-start = Label(root, text="Til (dd.mm.yyyy): ",bg="#5A6D7C",fg="white")
-end = Label(root, text="Frá (dd.mm.yyyy): ",bg="#5A6D7C",fg="white")
-
-#Create entry fields
-startInput = Entry(root, width=20, font=("Courier", 20)) #input for the date, should be on format 'dd.mm.yyyy'
-endInput = Entry(root, width=20, font=("Courier", 20)) #same ^^
 
 
 #Create buttons
+jeep = Button(root, text="1. Jeppar", bg="#424242", fg="white", width=22, height=2)
+small_car = Button(root, text="2. Smábílar", bg="#424242", fg="white", width=22, height=2)
+sedan = Button(root, text="3. Fólksbílar", bg="#424242", fg="white", width=22, height=2)
+luxury_car = Button(root, text="4. Lúxurbílar", bg="#424242", fg="white", width=22, height=2)
+all_cars = Button(root, text="5. Allir bílar", bg="#424242", fg="white", width=22, height=2)
 back = Button(root, text="Esc - Til baka", bg="#C8C8C8", fg="black", width=18, height=1)
-confirm = Button(root, text="Staðfesta", bg="#C8C8C8", fg="black", width=18, height=1)
 
-#################################################################################
+###
 
 #configure labels
 bilaleigaTinna.config(font=("Courier", 32))
 label1.config(font=("Courier", 24))
-start.config(font=("Courier", 24))
-end.config(font=("Courier", 24))
-confirm.config(font=("Courier", 16))
+jeep.config( font=("Courier",16))
+small_car.config( font=("Courier",16))
+sedan.config( font=("Courier",16))
+luxury_car.config( font=("Courier",16))
+all_cars.config( font=("Courier",16))
 back.config(font=("Courier",16))
 line1.config(font=("Courier", 28))
 line2.config(font=("Courier", 28))
 
 
-#Position widgets
 
 #positioning everything on the screen
 bilaleigaTinna.grid(row=1, column=3)
 label1.grid(row=3, column=3)
-start.grid(row=4, column=2)
-startInput.grid(row=4, column=3)
-end.grid(row=5, column=2)
-endInput.grid(row=5, column=3)
-confirm.grid(row=10, column=4)
-back.grid(row=10, column=2)
+jeep.grid(row=4, column=2)
+small_car.grid(row=4, column=3)
+sedan.grid(row=4, column=4)
+luxury_car.grid(row=8, column=2)
+all_cars.grid(row=8, column=3)
+back.grid(row=10, column=3)
 line1.grid(row=2,column=3)
 line2.grid(row=9,column=3)
  
-
 
 
 #position frame
@@ -68,8 +65,7 @@ root.grid_columnconfigure(0, weight=10)
 root.grid_columnconfigure(6, weight=10)
 
 
-
-#################################################################################
+#####
 
 
 root.mainloop()
