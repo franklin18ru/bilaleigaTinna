8ef5c3165a0c06408e100808b6053b44383b6680
Franklin Vale <franklin18@ru.is>
2018-12-06 14:22:47 +0000

    Working main.py file with the loginUI

--- GUI/loginUI.py
+++ GUI/loginUI.py
@@ -5,81 +5,83 @@ from tkinter import *
 
 from services.login import loginVerification
 
-def runLoginUi():
-    def verify():
-        verifyButton(usernameInput,passwordInput,root)
-    def verifyButton(usernameInput, passwordInput, root):
-        usernameInput = int(usernameInput.get())
-        passwordInput = passwordInput.get()
-        verifyLogin = loginVerification(usernameInput,passwordInput)
-        verification = verifyLogin.verify()
-
-        if verification == True:
-            success = Button(root, text="SUCCESS!", bg="black", fg="white",width=40, height=10)
-            success.grid(row=1, column=2)
-        else:
-            failed = Button(root, text="LOGIN FAILED!", bg="black", fg="white",width=40, height=10)
-            failed.grid(row=1, column=2)
-
-    #create frame
-    root = Tk()
-    root.title("Bílaleiga Tinna")
-    screen_width = root.winfo_screenwidth() #Gets the screen width
-    screen_height = root.winfo_screenheight() #Gets the screen height
-    root.configure(bg="#5A6D7C") # Changes background color of frame
-    root.geometry(str(screen_width)+"x"+str(screen_height)) #Sets the size of frame
+def main():
+    verification = False
+    def runLoginUi():
+        def verify():
+            verifyButton(usernameInput,passwordInput,root)
+        def verifyButton(usernameInput, passwordInput, root):
+            global verification
+            usernameInput = int(usernameInput.get())
+            passwordInput = passwordInput.get()
+            verifyLogin = loginVerification(usernameInput,passwordInput)
+            verification = verifyLogin.verify()
+            if verification == False:
+                error = Button(root, text="Rangt starfsnúmer eða lykilorð", bg="red", fg="white",font=("Courier",14))
+                error.grid(row=8,column=2)
 
 
-
-    #Create labels
-    bilaleigaTinna = Label(root, text="Bílaleiga Tinna",bg="#5A6D7C",fg="white")
-    label1 = Label(root, text="Innskráning",bg="#5A6D7C",fg="white")
-    username = Label(root, text="Starfsnúmer:",bg="#5A6D7C",fg="white")
-    password = Label(root, text="Lykilorð:",bg="#5A6D7C",fg="white")
-
-
-
-    #Create the entry fields
-    usernameInput = Entry(root, width=20, font=("Courier", 20))
-    passwordInput = Entry(root, show="*", width=20, font=("Courier", 20))
+        
+        #create frame
+        root = Tk()
+        root.title("Bílaleiga Tinna")
+        screen_width = root.winfo_screenwidth() #Gets the screen width
+        screen_height = root.winfo_screenheight() #Gets the screen height
+        root.configure(bg="#5A6D7C") # Changes background color of frame
+        root.geometry(str(screen_width)+"x"+str(screen_height)) #Sets the size of frame
 
 
-    #Create buttons
-    confirm = Button(root, text="Innskrá", bg="#424242", fg="white", width=8, height=1,font=("Courier",16), command=verify)
-
-    #################################################################################
-
-    #configure labels
-    bilaleigaTinna.config(font=("Courier", 32))
-    label1.config(font=("Courier", 28))
-    username.config(font=("Courier", 22))
-    password.config(font=("Courier", 22))
+        #Create labels
+        bilaleigaTinna = Label(root, text="Bílaleiga Tinna",bg="#5A6D7C",fg="white")
+        label1 = Label(root, text="Innskráning",bg="#5A6D7C",fg="white")
+        username = Label(root, text="Starfsnúmer:",bg="#5A6D7C",fg="white")
+        password = Label(root, text="Lykilorð:",bg="#5A6D7C",fg="white")
 
 
 
-    #Position widgets
-
-    #labels
-    bilaleigaTinna.grid(row=1, column=2, sticky = NSEW)
-    label1.grid(row=3, column=2, sticky = NSEW)
-    username.grid(row=4, column=0, sticky = E)
-    password.grid(row=5, column=0, sticky = E)
-
-    #Entries
-    usernameInput.grid(row=4, column=2)
-    passwordInput.grid(row=5, column=2)
-    confirm.grid(row=6, column=2)
-
-    #position frame
-    root.grid_rowconfigure(0, weight=1)
-    root.grid_rowconfigure(2, weight=1)
-    root.grid_rowconfigure(7, weight=3)
-    root.grid_columnconfigure(0, weight=1)
-    root.grid_columnconfigure(7, weight=1)
+        #Create the entry fields
+        usernameInput = Entry(root, width=20, font=("Courier", 20))
+        passwordInput = Entry(root, show="*", width=20, font=("Courier", 20))
 
 
-    #################################################################################
+        #Create buttons
+        confirm = Button(root, text="Innskrá", bg="#424242", fg="white", width=8, height=1,font=("Courier",16), command=verify)
+        #################################################################################
+
+        #configure labels
+        bilaleigaTinna.config(font=("Courier", 32))
+        label1.config(font=("Courier", 28))
+        username.config(font=("Courier", 22))
+        password.config(font=("Courier", 22))
 
 
-    root.mainloop() 
-runLoginUi()
+
+        #Position widgets
+
+        #labels
+        bilaleigaTinna.grid(row=1, column=2, sticky = NSEW)
+        label1.grid(row=3, column=2, sticky = NSEW)
+        username.grid(row=4, column=0, sticky = E)
+        password.grid(row=5, column=0, sticky = E)
+
+        #Entries
+        usernameInput.grid(row=4, column=2)
+        passwordInput.grid(row=5, column=2)
+        confirm.grid(row=6, column=2)
+
+        #position frame
+        root.grid_rowconfigure(0, weight=1)
+        root.grid_rowconfigure(2, weight=1)
+        root.grid_rowconfigure(7, weight=1)
+        root.grid_rowconfigure(9, weight=3)
+        root.grid_columnconfigure(0, weight=1)
+        root.grid_columnconfigure(9, weight=1)
+
+
+        #################################################################################
+
+
+        root.mainloop()
+
+    runLoginUi()
+    return verification
--- main/main.py
+++ main/main.py
@@ -1,3 +1,8 @@
-from gui.loginUI import runLoginUi
-from gui.services import login
-runLoginUi()
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..')) #The golden import line (No changing plz)
+from GUI.loginUI import main
+
+
+login = main()
+
