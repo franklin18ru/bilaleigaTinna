23f60620e87246c575f00c6acbf4541b899072de
olafur98 <olafurb18@ru.is>
2018-12-06 20:22:31 +0000

    adding

--- data_access/deleteCustomer.py
+++ data_access/deleteCustomer.py
@@ -2,7 +2,10 @@ import csv
 
 def deleteCustomer(name,ssn):
     with open("data/customers.csv","r+") as openfile:
-        for line in openfile:
-            print(line)
+        csv_reader = csv.reader(openfile)
+        next(csv_reader)
+        for line in csv_reader:
+            if name == line[1]:
+                pass
         
-deleteCustomer("bla","bla")
+deleteCustomer("Baldur","bla")
