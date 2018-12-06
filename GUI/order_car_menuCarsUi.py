7df23b6475feb6daa76096f10adbb96a46bc9156
mariamjoll1 <mariahrafns@gmail.com>
2018-12-06 21:02:03 +0000

    some updates on order_car_menuCarsUi

--- GUI/order_car_menuCarsUi.py
+++ GUI/order_car_menuCarsUi.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+import csv
 #create frame
 root = Tk()
 root.title("Bílaleiga Tinna")
@@ -9,84 +9,59 @@ root.configure(bg="#5A6D7C")
 root.geometry(str(screen_width)+"x"+str(screen_height))
 
 
+
 #Create labels
 bilaleigaTinna = Label(root, text="Bílaleiga Tinna",bg="#5A6D7C",fg="white")
 
-
 # A lable that should change for what the user clicks on:
-label1 = Label(root, text="Bílar",bg="#5A6D7C",fg="white") #setting a default value
-label1.config(font=("Courier", 24)) #setting font
-#here there should be an if statement to change the lable to the correct word:
-label1.config(text="Jeppar") #changing the default value
-label1.grid(row=3, column=3) #printing new value
+label2 = Label(root, text="Bílar",bg="#5A6D7C",fg="white") #setting the default value as Bílar
 
-
-
-
-cars = ["Toyoya Hilux", "Suzuki", "Range Rover", "G-Class", "Apa bíll"]
-row_num = 3
-column_num = 2
-counter = 0
-for item in cars:
-    label1 = Button(root, text=item ,bg="#424242",fg="white", width=22, height=2)
-    label1.config(font=("Courier", 16))
-    label1.grid(row = row_num, column=column_num)
-    counter += 1
-    column_num +=1
-    if counter == 3:
-        row_num += 1
-        counter = 0
-        column_num = 2
-
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
+# Here there should be an if statement to change the lable to the correct word:
+label2.config(text="Jeppar") #changing the default value
 
 line1 = Label(root, text="____________________________",bg="#5A6D7C",fg="white")
 line2 = Label(root, text="____________________________",bg="#5A6D7C",fg="white")
+back = Button(root, text="Esc - Til baka", bg="#C8C8C8", fg="black", width=18, height=1)
 
 
-# #Create buttons
-# jeep = Button(root, text="1. Jeppar", bg="#424242", fg="white", width=22, height=2)
-# small_car = Button(root, text="2. Smábílar", bg="#424242", fg="white", width=22, height=2)
-# sedan = Button(root, text="3. Fólksbílar", bg="#424242", fg="white", width=22, height=2)
-# luxury_car = Button(root, text="4. Lúxurbílar", bg="#424242", fg="white", width=22, height=2)
-# all_cars = Button(root, text="5. Allir bílar", bg="#424242", fg="white", width=22, height=2)
-# back = Button(root, text="Esc - Til baka", bg="#C8C8C8", fg="black", width=18, height=1)
+#Open the cars file and printing the content in buttons
+with open('data/cars.csv', 'r', newline="") as cars:
+    csv_reader = csv.reader(cars)
+    next(csv_reader)
+    row_num = 4
+    column_num = 2
+    counter = 0
+    for item in csv_reader:
+        label1 = Button(root, text=item[2] ,bg="#424242",fg="white", width=22, height=2)
+        label1.config(font=("Courier", 16))
+        label1.grid(row = row_num, column=column_num)
+        counter += 1
+        column_num += 1
+        if counter == 3:
+            row_num += 1
+            counter = 0
+            column_num = 2
 
-# ###
 
-# #configure labels
+# configure labels
 bilaleigaTinna.config(font=("Courier", 32))
-# label1.config(font=("Courier", 24))
-# jeep.config( font=("Courier",16))
-# small_car.config( font=("Courier",16))
-# sedan.config( font=("Courier",16))
-# luxury_car.config( font=("Courier",16))
-# all_cars.config( font=("Courier",16))
-# back.config(font=("Courier",16))
+back.config(font=("Courier",16))
 line1.config(font=("Courier", 28))
 line2.config(font=("Courier", 28))
+label2.config(font=("Courier", 26)) #the changable label
 
 
 
-# #positioning everything on the screen
+# positioning everything on the screen
 bilaleigaTinna.grid(row=1, column=3)
-# label1.grid(row=3, column=3)
-# jeep.grid(row=4, column=2)
-# small_car.grid(row=4, column=3)
-# sedan.grid(row=4, column=4)
-# luxury_car.grid(row=8, column=2)
-# all_cars.grid(row=8, column=3)
-# back.grid(row=10, column=3)
+label2.grid(row=3,column=3)
+back.grid(row=0, column=2)
 line1.grid(row=2,column=3)
 line2.grid(row=9,column=3)
  
 
 
-# #position frame
+# position frame
 root.grid_rowconfigure(0, weight=3)
 root.grid_rowconfigure(1, weight=0)
 root.grid_rowconfigure(3, weight=1)
@@ -97,7 +72,7 @@ root.grid_columnconfigure(0, weight=10)
 root.grid_columnconfigure(6, weight=10)
 
 
-# #####
+#####
 
 
 root.mainloop()
