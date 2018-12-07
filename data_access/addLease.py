9eb9520cd117eb5e95fe34748a91f67037d5f4f7
olafur98 <olafurb18@ru.is>
2018-12-07 14:19:40 +0000

    Compile

--- data_access/addLease.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-def addLease(ssn,renter,leasePeriod,licensePlate):
-    newLease=[ssn,renter,leasePeriod,licensePlate]
-    with open('data/cars.csv', 'a',newline="") as openfile:
-        csv_writer = csv.writer(openfile)
-        csv_writer.writerow(newLease) 
